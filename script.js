2c36020794b5b4048c512a57d8488c427406d98d
Víctor J. González <vgonzalez165@gmail.com>
2024-12-24 18:15:34 +0100

    xx

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 const targetDate = new Date("2024-12-26T16:00:00");
+// const targetDate = new Date("2024-12-24T18:11:00");
 
-    const interval = setInterval(() => {
-      const now = new Date(); 
-      const timeDifference = targetDate - now; 
+const interval = setInterval(() => {
+    const now = new Date(); 
+    const timeDifference = targetDate - now; 
 
-      if (timeDifference > 0) {
-        const hours = Math.floor((timeDifference / (1000 * 60 * 60)));
-        const minutes = Math.floor((timeDifference / (1000 * 60)) % 60);
-        const seconds = Math.floor((timeDifference / 1000) % 60);
+    if (timeDifference > 0) {
+    const hours = Math.floor((timeDifference / (1000 * 60 * 60)));
+    const minutes = Math.floor((timeDifference / (1000 * 60)) % 60);
+    const seconds = Math.floor((timeDifference / 1000) % 60);
 
-        document.getElementById("timer").textContent = `
-          ${hours.toString().padStart(2, '0')}h :
-          ${minutes.toString().padStart(2, '0')}m :
-          ${seconds.toString().padStart(2, '0')}s
-        `;
-      } else {
-        // Detiene el cronómetro cuando el tiempo se agota
-        clearInterval(interval);
-        document.getElementById("countdown").textContent = "¡Llegó el momento!";
-      }
-    }, 1000);
+    document.getElementById("timer").textContent = `
+        ${hours.toString().padStart(2, '0')}h :
+        ${minutes.toString().padStart(2, '0')}m :
+        ${seconds.toString().padStart(2, '0')}s
+    `;
+    } else {
+    // Detiene el cronómetro cuando el tiempo se agota
+    clearInterval(interval);
+    document.getElementById("container").style.display = "block";
+    }
+}, 1000);
+
+document.get
