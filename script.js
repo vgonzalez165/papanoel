c58665fe237055eadea683535fee88b60af9012b
Víctor J. González <vgonzalez165@gmail.com>
2024-12-24 08:53:42 +0100

    x

---
+++ script.js
@@ -0,0 +1,22 @@
+const targetDate = new Date("2024-12-26T16:00:00");
+
+    const interval = setInterval(() => {
+      const now = new Date(); 
+      const timeDifference = targetDate - now; 
+
+      if (timeDifference > 0) {
+        const hours = Math.floor((timeDifference / (1000 * 60 * 60)));
+        const minutes = Math.floor((timeDifference / (1000 * 60)) % 60);
+        const seconds = Math.floor((timeDifference / 1000) % 60);
+
+        document.getElementById("timer").textContent = `
+          ${hours.toString().padStart(2, '0')}h :
+          ${minutes.toString().padStart(2, '0')}m :
+          ${seconds.toString().padStart(2, '0')}s
+        `;
+      } else {
+        // Detiene el cronómetro cuando el tiempo se agota
+        clearInterval(interval);
+        document.getElementById("countdown").textContent = "¡Llegó el momento!";
+      }
+    }, 1000);
